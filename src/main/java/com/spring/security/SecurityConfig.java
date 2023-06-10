2eefcdd9d3c5fcb1e9fcd5b404557551f3024422
AnhTu <3artemis98@gmail.com>
2023-06-10 22:22:26 +0700

    update login

--- src/main/java/com/spring/security/SecurityConfig.java
+++ src/main/java/com/spring/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.spring.security;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +16,21 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/").permitAll()
-		.anyRequest().authenticated().and()
-		.formLogin().loginPage("/login").permitAll();
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		//.loginPage("/login")
+        .defaultSuccessUrl("/")
+        .permitAll();
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users=User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+		.withUser(users.username("john").password("123456").roles("employee"))
+		.withUser(users.username("mary").password("123456").roles("manager"))
+		.withUser(users.username("susan").password("123456").roles("admin"));
+
+
 	}
 }
--- src/main/webapp/WEB-INF/view/login.jsp
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -8,5 +8,19 @@
 </head>
 <body>
 <h1>login</h1>
+
+<form action="/login" method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <input type="submit" value="Login">
+        </div>
+    </form>
 </body>
 </html>
